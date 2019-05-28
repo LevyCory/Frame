9063d17d21bbe78ba201121b767941035da80cc1
= <corylevy@icloud.com>
2019-05-29 00:14:25 +0300

    Added the catch library and written the first test case

--- Tests/loader_tests.cpp
+++ Tests/loader_tests.cpp
@@ -1,3 +1,11 @@
-#include "catch.h"
-#include "frame_status.h"
-#include "loader.h"
+#define CATCH_CONFIG_MAIN
+
+#include <catch.h>
+#include <frame_status.h>
+#include <loader.h>
+
+TEST_CASE("Test LOADER_LoadLibrary invalid args", "[loader]")
+{
+	FRAMESTATUS eStatus = LOADER_LoadLibrary(NULL, NULL);
+	REQUIRE(FRAMESTATUS_LOADER_LOADLIBRARY_INVALID_PARAMETERS == eStatus);
+}
